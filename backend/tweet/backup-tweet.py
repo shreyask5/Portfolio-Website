7f1565705789886b007c2a5db186162ba1353d85
Shreyas K <shreyasksh5@gmail.com>
2024-09-06 13:31:20 +0530

    Tweet Version 2 with quote Images

--- backend/tweet/backup-tweet.py
+++ backend/tweet/backup-tweet.py
@@ -136,4 +136,4 @@ def analyze_tweet_route():
 
 if __name__ == '__main__':
     asyncio.run(initialize_client())
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)

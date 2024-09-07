d702a1dd913c1c1c1a77d733243014306aec8818
Shreyas K <shreyasksh5@gmail.com>
2024-09-07 11:26:43 +0530

    404 Page Nginx Update Fix

--- backend/tweet/tweet.py
+++ backend/tweet/tweet.py
@@ -108,7 +108,7 @@ async def analyze_tweet(url):
 # Define a Flask route to handle the tweet analysis
 @app.route("/projects/tweet-analyzer/")
 def hello():
-    return render_template('project-1.html')
+    return render_template('/home/ubuntu/frontend/web/project-1.html')
 
 @app.route('/projects/api/', methods=['POST'])
 def analyze_tweet_route():

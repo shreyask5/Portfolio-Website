fb6e1ee833e41f051615135664cb959c52f71668
Shreyas K <shreyasksh5@gmail.com>
2024-09-17 01:02:10 +0530

    Admin Login Test

--- backend/php-testing/login.php
+++ backend/php-testing/login.php
@@ -31,6 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION["username"] = $username;
             header("Location: adminhome.php");
             exit;
+            ob_end_flush();
         } else {
             echo "Invalid username or password.";
         }

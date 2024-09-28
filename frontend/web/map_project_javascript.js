ca203af8eb089c891aa34ab6bd17e276bf69ea30
Shreyas K <shreyasksh5@gmail.com>
2024-09-28 21:45:33 +0530

    Restaurant Waiting Time

--- frontend/web/map_project_javascript.js
+++ frontend/web/map_project_javascript.js
@@ -174,7 +174,6 @@ let slideTimer;
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -183,12 +182,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 
   slideTimer = setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
@@ -206,22 +200,15 @@ function plusSlides(n) {
 function showSlidesManually() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
   if (slideIndex > slides.length) {slideIndex = 1}
   if (slideIndex < 1) {slideIndex = slides.length}
 
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  
+  slides[slideIndex-1].style.display = "block";   
   slideTimer = setTimeout(showSlides, 2000); // Restart the automatic slideshow
 }
 

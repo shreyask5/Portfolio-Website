4d58f3ac0764e6dccb8112072ff816250000764f
Shreyas K <shreyasksh5@gmail.com>
2024-10-13 20:54:20 +0530

    Fixing Bargraph

--- frontend/web/map_project_javascript.js
+++ frontend/web/map_project_javascript.js
@@ -162,7 +162,7 @@ async function displayPlaceDetails(place) {
 
     const chartData = waitTimeData.converted_data;
     const ctx = document.getElementById('busyChart').getContext('2d');
-    let busyChart = new Chart(ctx, {
+    window.busyChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: [,'12a','1a','2a','3a','4a','5a','6a','7a','8a','9a', '10a', '11a', '12p', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p','11p'],
@@ -182,11 +182,6 @@ async function displayPlaceDetails(place) {
             }
         }
     });
-
-    function showChart(day) {
-        busyChart.data.datasets[0].data = chartData[day];
-        busyChart.update();
-    }
 }
 
 function generateStars(rating, starContainer) {
@@ -258,3 +253,8 @@ function showSlidesManually() {
 
 window.onload = initMap;
 showSlides();
+
+function showChart(day) {
+    window.busyChart.data.datasets[0].data = chartData[day];
+    window.busyChart.update();
+}

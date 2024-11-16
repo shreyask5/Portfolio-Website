8df1679b5f29279a14834c8d3b25b2a6254643a1
shreyask5 <shreyasksh5@gmail.com>
2024-11-16 15:34:28 +0530

    Tweet Analyzer 2.0

--- backend/tweet/fastapi-tweet.py
+++ backend/tweet/fastapi-tweet.py
@@ -149,4 +149,4 @@ async def analyze_tweet_route(request: Request):
 
 if __name__ == '__main__':
     asyncio.run(initialize_client())
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
